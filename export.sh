88b76c268c95c4226fdd80675f880dfa02350168
ae <90932525+ea-ae@users.noreply.github.com>
2023-10-03 00:28:56 +0300

    Update conf

--- export.sh
+++ export.sh
@@ -6,6 +6,6 @@ cp ~/.gitconfig ./.gitconfig
 cp ~/.config/nvim/init.vim ./init.vim
 cp ~/.vimrc ./.vimrc 
 
-cp ${APPDATA}/Code/User/settings.json ./vscode/settings.jsonc
-cp ${APPDATA}/Code/User/keybindings.json ./vscode/keybindings.jsonc
+cp ${APPDATA}/Code/User/settings.json vscode.settings.jsonc
+cp ${APPDATA}/Code/User/keybindings.json vscode.keybindings.jsonc
 
