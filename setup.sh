bf633e75ba7797545939bb5d0c0f381f5a8cee5b
ae <90932525+ea-ae@users.noreply.github.com>
2023-10-08 02:45:32 +0300

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+### Install tools
+
+choco install bat  # better cat, syntax highlighting
+choco install delta # better difftool
+choco install fd # faster find
+choco install fzf # fuzzy finder (preview with bat)
+choco install neovim # better vim
+choco install pnpm # better npm
+choco install ripgrep # faster grep
