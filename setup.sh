918e54f605252875d4aa691bb3db6f0879369857
ae <90932525+ea-ae@users.noreply.github.com>
2023-10-08 02:50:05 +0300

    Update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
-### Install tools
+### tools
 
 choco install bat     # better cat, syntax highlighting
 choco install delta   # better difftool
-choco install fd      # faster find
-choco install fzf     # fuzzy finder (preview with bat)
+choco install fd      # better find
+choco install fzf     # fuzzy finder
 choco install neovim  # better vim
 choco install pnpm    # better npm
-choco install ripgrep # faster grep
+choco install ripgrep # better grep
+choco install sd-cli  # better sed
